21ba92e4d9dcbc6d82819782d238199a823a96d4
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-10-04 22:50:43 -0500

    Initial commit

--- DevMtn/Js-Interactivity/js-interactivity-demo/solution/index.js
+++ DevMtn/Js-Interactivity/js-interactivity-demo/solution/index.js
@@ -1,8 +1,4 @@
-// checking logs in the console
-
 console.log('hello world')
-
-// below is to change the counter
 
 const counterText = document.querySelector('#counter')
 
@@ -11,8 +7,6 @@ const resetBtn = document.querySelector('#reset-btn')
 const plusBtn = document.querySelector('#plus-btn')
 
 let count = 0;
-
-console.log(counterText);
 
 function increase() {
     count++
@@ -37,8 +31,6 @@ minusBtn.addEventListener('click', decrease)
 resetBtn.addEventListener('click', reset)
 plusBtn.addEventListener('click', increase)
 
-// below is to change themes
-
 const themeBtns = document.querySelectorAll('.theme-buttons')
 
 for (let i = 0; i < themeBtns.length; i++) {
@@ -56,26 +48,3 @@ function selectTheme(event) {
         buttons[i].className = theme;
     }
 }
-
-// below is to create the food list
-
-const inputField = document.querySelector('#food-input');
-const foodBtn = document.querySelector('#food-btn');
-const foodList = document.querySelector('#food-list');
-
-let newFood = '';
-
-function newFoodHandler(event) {
-    newFood = event.target.value;
-}
-
-function addNewFood() {
-    let newPara = document.createElement('p');
-    newPara.textContent = newFood;
-    foodList.appendChild(newPara);
-}
-
-console.log(foodList);
-
-inputField.addEventListener('change', newFoodHandler);
-foodBtn.addEventListener('click', addNewFood);
